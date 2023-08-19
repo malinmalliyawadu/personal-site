415db28483eeb370cbb0e9b0b904552753608760
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2023-08-19 15:12:04 +1200

    Add wave animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        blob: "blob 60s infinite ease-in",
+        blob: "blob 60s infinite",
+        wave: "wave 2.5s infinite",
       },
       keyframes: {
         blob: {
@@ -16,6 +17,32 @@ module.exports = {
           },
           "100%": {
             transform: "rotate(0deg) translate(-150px) scale(1.2)",
+          },
+        },
+        wave: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "10%": {
+            transform: "rotate(14deg)",
+          },
+          "20%": {
+            transform: "rotate(-8deg)",
+          },
+          "30%": {
+            transform: "rotate(14deg)",
+          },
+          "40%": {
+            transform: "rotate(-4deg)",
+          },
+          "50%": {
+            transform: "rotate(10deg)",
+          },
+          "60%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(0deg)",
           },
         },
       },
